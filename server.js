4e64ea41c1eab4df8f513fc656b7a426da383fe8
Olha Shabanova <shabanova994@gmail.com>
2025-03-26 23:15:01 +0200

    Try to fix cors

--- server.js
+++ server.js
@@ -5,24 +5,12 @@ const PORT = process.env.PORT || 3000;
 const pool = require('./db')
 
 const corsOptions = {
-    origin: [
-        'http://localhost:3001',
-        'https://questionnaire-app-client-63ff70b3d682.herokuapp.com/'
-    ],
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: 'https://questionnaire-app-client-63ff70b3d682.herokuapp.com/',
     credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(204); // No Content
-});
 
 app.get('/quizzes', async (req, res) => {
     try {
@@ -34,9 +22,6 @@ app.get('/quizzes', async (req, res) => {
                    completionCount as "completionCount"
             FROM quizzes`
         );
-        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.json(result.rows);
     } catch (error) {
         console.error('Error fetching quizzes:', error);
@@ -44,20 +29,10 @@ app.get('/quizzes', async (req, res) => {
     }
 });
 
-app.options('/quizzes/:id', (req, res) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(204); // No Content
-});
-
 app.delete('/quizzes/:id', async (req, res) => {
     try {
         const { id } = req.params
         const result = await pool.query(`DELETE FROM quizzes WHERE id = $1`, [id]);
-        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         if (result.rowCount === 0) return res.status(404).json({ message: 'Item not found' });
         res.status(200).json({ message: `Item ${id} deleted` });
     } catch (error) {
@@ -67,5 +42,5 @@ app.delete('/quizzes/:id', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT} port`);
 });
